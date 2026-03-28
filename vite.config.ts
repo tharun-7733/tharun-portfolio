162d1948dabef6e84a12f575203a427249cb2d34
tharun-7733 <tharuntej7373@gmail.com>
2026-03-28 16:46:52 +0530

    fix: vite build fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./",   // important for your custom domain
+  base: "/",   // important for your custom domain
 
   build: {
     outDir: "dist", 

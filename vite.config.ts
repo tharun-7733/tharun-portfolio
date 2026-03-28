82a54ea4f1e4f55e9b9a10bd7b04b6eec51f5d91
tharun-7733 <tharuntej7373@gmail.com>
2026-03-28 13:30:57 +0530

    change assets

--- vite.config.ts
+++ vite.config.ts
@@ -2,20 +2,17 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  root: "src",
+export default defineConfig({
+  plugins: [react()],
+  base: "/",   // important for your custom domain
+
   build: {
-    outDir: "../",
+    outDir: "dist", 
   },
-  plugins: [react()].filter(Boolean),
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
